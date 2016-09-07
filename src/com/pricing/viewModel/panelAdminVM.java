33649b02d767641fa823dc9b933df9e5c3e37967
francito1234 <desarrollo1@eddyonsoft.com>
2016-09-07 10:48:08 -0500

    actualizando clases

--- src/com/pricing/viewModel/panelAdminVM.java
+++ src/com/pricing/viewModel/panelAdminVM.java
@@ -5,12 +5,13 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import org.zkoss.bind.BindUtils;
 import org.zkoss.bind.annotation.AfterCompose;
 import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.ContextParam;
 import org.zkoss.bind.annotation.ContextType;
+import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.util.media.Media;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.WrongValueException;
@@ -27,12 +28,333 @@ import org.zkoss.zul.Textbox;
 import com.pricing.extras.lectorPDF;
 import com.pricing.util.Util;
 
-public class panelAdminVM 
+public class panelAdminVM
 {
 	@Wire
 	Textbox txtNombreArchivo;
 	@Wire
 	Label lblMensaje;
+	//==============VARIABLES INTERFACES==============
+	private boolean visibleEtiqueta=false;
+	private boolean visiblePaquetes=false;
+	private boolean visibleServicios=false;
+	private boolean visibleSubServicios=false;
+	private boolean visibleImpuestos=false;
+	private boolean visibleDisponibilidad=false;
+	
+	//===============VARIABLES SUBITEMS================
+	private boolean openItemConfig;
+	private boolean openItemUsuarios;
+	private boolean openItemSliders;
+	private boolean openItemPaquetes;
+	
+	//===============VARIABLES RESPONSIVE===========
+	private boolean visibleMenu = false;
+	private String windowMode = "embedded";
+	private boolean visibleBoton= false;
+	private String nombres="";
+	private boolean visibleTabbox=false;
+	
+	//===============SELECCION TABS==================
+	private boolean seleccionDisponibilidad=false;
+	private boolean seleccionEtiquetas=false;
+	private boolean seleccionPaquetes=false;
+	private boolean seleccionServicios=false;
+	private boolean seleccionSubServicios=false;
+	private boolean seleccionImpuestos=false;
+	
+	//=================GET Y SET SELECCION TABS=======
+	public boolean isSeleccionDisponibilidad() {
+		return seleccionDisponibilidad;
+	}
+
+	public void setSeleccionDisponibilidad(boolean seleccionDisponibilidad) {
+		this.seleccionDisponibilidad = seleccionDisponibilidad;
+	}
+
+	public boolean isSeleccionEtiquetas() {
+		return seleccionEtiquetas;
+	}
+
+	public void setSeleccionEtiquetas(boolean seleccionEtiquetas) {
+		this.seleccionEtiquetas = seleccionEtiquetas;
+	}
+
+	public boolean isSeleccionPaquetes() {
+		return seleccionPaquetes;
+	}
+
+	public void setSeleccionPaquetes(boolean seleccionPaquetes) {
+		this.seleccionPaquetes = seleccionPaquetes;
+	}
+
+	public boolean isSeleccionServicios() {
+		return seleccionServicios;
+	}
+
+	public void setSeleccionServicios(boolean seleccionServicios) {
+		this.seleccionServicios = seleccionServicios;
+	}
+
+	public boolean isSeleccionSubServicios() {
+		return seleccionSubServicios;
+	}
+
+	public void setSeleccionSubServicios(boolean seleccionSubServicios) {
+		this.seleccionSubServicios = seleccionSubServicios;
+	}
+
+	public boolean isSeleccionImpuestos() {
+		return seleccionImpuestos;
+	}
+
+	public void setSeleccionImpuestos(boolean seleccionImpuestos) {
+		this.seleccionImpuestos = seleccionImpuestos;
+	}
+	//================RESPONSIVE======================
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	
+	public boolean isVisibleTabbox() {
+		return visibleTabbox;
+	}
+
+	public void setVisibleTabbox(boolean visibleTabbox) {
+		this.visibleTabbox = visibleTabbox;
+	}
+	
+	public boolean isVisibleMenu() {
+		return visibleMenu;
+	}
+
+	public void setVisibleMenu(boolean visibleMenu) {
+		this.visibleMenu = visibleMenu;
+	}
+
+	public String getWindowMode() {
+		return windowMode;
+	}
+
+	public void setWindowMode(String windowMode) {
+		this.windowMode = windowMode;
+	}
+
+	public boolean isVisibleBoton() {
+		return visibleBoton;
+	}
+
+	public void setVisibleBoton(boolean visibleBoton) {
+		this.visibleBoton = visibleBoton;
+	}
+	//================CAMBIOS ITEMS==============
+	public boolean isOpenItemConfig() {
+		return openItemConfig;
+	}
+
+	public void setOpenItemConfig(boolean openItemConfig) {
+		this.openItemConfig = openItemConfig;
+	}
+
+	public boolean isOpenItemUsuarios() {
+		return openItemUsuarios;
+	}
+
+	public void setOpenItemUsuarios(boolean openItemUsuarios) {
+		this.openItemUsuarios = openItemUsuarios;
+	}
+
+	public boolean isOpenItemSliders() {
+		return openItemSliders;
+	}
+
+	public void setOpenItemSliders(boolean openItemSliders) {
+		this.openItemSliders = openItemSliders;
+	}
+
+	public boolean isOpenItemPaquetes() {
+		return openItemPaquetes;
+	}
+
+	public void setOpenItemPaquetes(boolean openItemPaquetes) {
+		this.openItemPaquetes = openItemPaquetes;
+	}
+	//===============CAMBIO INTERFACES==============
+	public boolean isVisibleDisponibilidad() {
+		return visibleDisponibilidad;
+	}
+
+	public void setVisibleDisponibilidad(boolean visibleDisponibilidad) {
+		this.visibleDisponibilidad = visibleDisponibilidad;
+	}
+	
+	public boolean isVisibleEtiqueta() {
+		return visibleEtiqueta;
+	}
+
+	public void setVisibleEtiqueta(boolean visibleEtiqueta) {
+		this.visibleEtiqueta = visibleEtiqueta;
+	}
+
+	public boolean isVisiblePaquetes() {
+		return visiblePaquetes;
+	}
+
+	public void setVisiblePaquetes(boolean visiblePaquetes) {
+		this.visiblePaquetes = visiblePaquetes;
+	}
+
+	public boolean isVisibleServicios() {
+		return visibleServicios;
+	}
+
+	public void setVisibleServicios(boolean visibleServicios) {
+		this.visibleServicios = visibleServicios;
+	}
+
+	public boolean isVisibleSubServicios() {
+		return visibleSubServicios;
+	}
+
+	public void setVisibleSubServicios(boolean visibleSubServicios) {
+		this.visibleSubServicios = visibleSubServicios;
+	}
+
+	public boolean isVisibleImpuestos() {
+		return visibleImpuestos;
+	}
+
+	public void setVisibleImpuestos(boolean visibleImpuestos) {
+		this.visibleImpuestos = visibleImpuestos;
+	}
+
+	@Init
+	public void Inicializar() {
+		seleccionDisponibilidad=seleccionEtiquetas=seleccionImpuestos=seleccionPaquetes=seleccionServicios=seleccionSubServicios=false;
+		visibleDisponibilidad=visibleEtiqueta = visiblePaquetes = visibleServicios = visibleSubServicios = visibleImpuestos =false;
+	}
+
+	//================CAMBIO DE VISIBILIDAD========
+	@Command
+	@NotifyChange({ "visibleEtiqueta", "visiblePaquetes", "visibleServicios", "visibleSubServicios", "visibleImpuestos", "visibleMenu", "visibleDisponibilidad",
+		"seleccionDisponibilidad","seleccionEtiquetas", "seleccionPaquetes", "seleccionServicios", "seleccionSubServicios", "seleccionImpuestos" })
+	public void Cambio(@BindingParam("cambioInterfaz") String cambios) {
+		if(windowMode.equals("popup"))
+		{
+			if (cambios.equals("itemDisponibilidad")) {
+				visibleDisponibilidad = true; visibleMenu=false;
+			} else if (cambios.equals("itemEtiqueta")) {
+				visibleEtiqueta = true; visibleMenu=false; 
+			} else if (cambios.equals("itemPaquete")) {
+				visiblePaquetes = true; visibleMenu=false;
+			} else if (cambios.equals("itemServicio")) {
+				visibleServicios = true; visibleMenu=false;
+			} else if (cambios.equals("itemSubServicio")) {
+				visibleSubServicios = true; visibleMenu=false;
+			} else if (cambios.equals("itemImpuesto")) {
+				visibleImpuestos= true; visibleMenu=false;
+			}
+		}else {
+			if (cambios.equals("itemDisponibilidad")) {
+				visibleDisponibilidad = seleccionDisponibilidad=true; 
+				seleccionEtiquetas=seleccionImpuestos=seleccionPaquetes=seleccionServicios=seleccionSubServicios=false;
+			} else if (cambios.equals("itemEtiqueta")) {
+				visibleEtiqueta = seleccionEtiquetas=true;
+				seleccionDisponibilidad=seleccionImpuestos=seleccionPaquetes=seleccionServicios=seleccionSubServicios=false;
+			} else if (cambios.equals("itemPaquete")) {
+				visiblePaquetes = seleccionPaquetes=true;
+				seleccionEtiquetas=seleccionImpuestos=seleccionDisponibilidad=seleccionServicios=seleccionSubServicios=false;
+			} else if (cambios.equals("itemServicio")) {
+				visibleServicios = seleccionServicios=true;
+				seleccionEtiquetas=seleccionImpuestos=seleccionPaquetes=seleccionDisponibilidad=seleccionSubServicios=false;
+			} else if (cambios.equals("itemSubServicio")) {
+				visibleSubServicios = seleccionSubServicios=true;
+				seleccionEtiquetas=seleccionImpuestos=seleccionPaquetes=seleccionServicios=seleccionDisponibilidad=false;
+			} else if (cambios.equals("itemImpuesto")) {
+				visibleImpuestos= seleccionImpuestos=true;
+				seleccionEtiquetas=seleccionDisponibilidad=seleccionPaquetes=seleccionServicios=seleccionSubServicios=false;
+			}
+		} 
+	}
+	
+	//=================CERRRAR TABS===============
+	@Command
+	@NotifyChange({ "visibleEtiqueta", "visiblePaquetes", "visibleServicios", "visibleSubServicios", "visibleImpuestos","visibleMenu","nombres","visibleDisponibilidad"})
+	public void CerrarTab(@BindingParam("cerrarInterfaz") String cambios) {
+		if(windowMode.equals("popup"))
+		{
+			if (cambios.equals("tabDisponibilidad")) {
+				visibleDisponibilidad = false; visibleMenu=false;
+			} else if (cambios.equals("tabEtiqueta")) {
+				visibleEtiqueta = false; visibleMenu=false; 
+			} else if (cambios.equals("tabPaquete")) {
+				visiblePaquetes = false; visibleMenu=false;
+			} else if (cambios.equals("tabServicio")) {
+				visibleServicios = false; visibleMenu=false;
+			} else if (cambios.equals("tabSubServicio")) {
+				visibleSubServicios = false; visibleMenu=false;
+			} else if (cambios.equals("tabImpuesto")) {
+				visibleImpuestos= false; visibleMenu=false;
+			}
+		}else {
+			if (cambios.equals("tabDisponibilidad")) {
+				visibleDisponibilidad = false;
+			} else if (cambios.equals("tabEtiqueta")) {
+				visibleEtiqueta = false;
+			} else if (cambios.equals("tabPaquete")) {
+				visiblePaquetes = false;
+			} else if (cambios.equals("tabServicio")) {
+				visibleServicios = false;
+			} else if (cambios.equals("tabSubServicio")) {
+				visibleSubServicios = false;
+			} else if (cambios.equals("tabImpuesto")) {
+				visibleImpuestos= false;
+			}
+		} 
+	}
+	
+	//================CAMBIO DE APERTURA==========
+	@Command
+	@NotifyChange({ "openItemConfig","openItemUsuarios","openItemPaquetes" })
+	public void CambioApertura(@BindingParam("cambioApertura") String cambios) {
+		if (cambios.equals("itemConfiguracion")) {
+			if(openItemConfig){ openItemConfig=false;}
+			else {openItemConfig = true; openItemUsuarios = openItemPaquetes = false;}
+		} else if (cambios.equals("itemUsuarios")) {
+			if(openItemUsuarios){ openItemUsuarios=false;}
+			else {openItemUsuarios = true; openItemSliders= openItemConfig = openItemPaquetes = false;}
+		} else if (cambios.equals("itemPaquetes")) {
+			if(openItemPaquetes){ openItemPaquetes=false;}
+			else {openItemPaquetes = true; openItemConfig = openItemUsuarios = openItemSliders = false;}
+		}				
+	}
+	//==============CAMBIO RESPONSIVE=============
+	  @Command
+	  @NotifyChange({"visibleBoton","visibleMenu","windowMode"})
+	  public void muestra(@BindingParam("width")int ancho){
+			  if(ancho<700){visibleBoton=true;}
+			  else {visibleBoton=false; windowMode="embedded"; visibleMenu=true;}
+	  }
+	  
+	  @Command
+	  @NotifyChange({"visibleMenu","windowMode","openItemConfig","openItemUsuarios","openItemPaquetes"})
+	  public void menuDesplegable()
+	  {   
+		  openItemConfig=openItemUsuarios=openItemPaquetes=false;
+		  windowMode="popup"; 
+		  visibleMenu=true;		  
+	  }
+	  
+	  @Command
+	  @NotifyChange("visibleMenu")
+	  public void CerrarPopup()
+	  {
+		  visibleMenu=false;
+	  }
 	//==============CARGAR PDF====================
 		@Command
 		 public void uploadFile(@BindingParam("id") final String id) {
